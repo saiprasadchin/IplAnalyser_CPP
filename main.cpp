c1bfd98fbe84c2dfaa308752b104188ad2e5a905
“saiprasadchin” <saiprasadchindam@gmail.com>
2020-09-10 07:33:34 -0700

    Added : Welcome message

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+void displayWelcomeMessage()
+{
+    cout << "\nWelcome to IplAnalyser Program\n" << endl;
+}
+
+int main()
+{
+    displayWelcomeMessage();
+	return 0;
+}
